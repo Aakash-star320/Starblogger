00199d42d95c47aa463da14887456573d33f20ee
Aakash-star320 <aakashsingh90per@gmail.com>
2025-01-08 07:39:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const __dirname=dirname(fileURLToPath(import.meta.url));
 
 let redisClient = createClient({
   socket: {
-    host: red-ctutmm9u0jms73aq01l0, // Change this if Redis is on a different host
-    port: 6379,        // Replace with the correct port if Redis is on a different port
+    host: process.env.REDIS_HOST, // Change this if Redis is on a different host
+    port: process.env.REDIS_PORT,        // Replace with the correct port if Redis is on a different port
     timeout:10000,
   },});
 redisClient.connect().catch(console.error);
